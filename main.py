40a99c14f6108c4108b48fd14fd78dd557225c37
Sofia Li <liiiijingwei@gmail.com>
2023-02-09 16:57:11 -0600

    new experiments

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 
 def decreasing(input):
-    input.sort()
+    return sorted(input, reverse=True)
 
 '''
 input style: a list of objects with only weights (no numbering)
@@ -44,15 +44,65 @@ def first_fit_decreasing(input):
 def best_fit_decreasing(input):
     return best_fit(decreasing(input))
 
-def generate_random(n):
-    return np.random.randint(1, 101, n) / 100;
+def generate_uniform(n):
+    return np.random.rand(n)
+
+def generate_cubed(n):
+    rand_array = generate_uniform(n)
+    return rand_array * rand_array * rand_array
+
+def generate_square_rooted(n):
+    rand_array = generate_uniform(n)
+    return np.sqrt(rand_array)
+
+def generate_centered(n):
+    rand_array = generate_uniform(n)
+    for i in range(n):
+        rand_array[i] = ((rand_array[i] - 0.5) ** 3) * 4 + 0.5
+    return rand_array
+
 
 
 if __name__ == '__main__':
-    input = generate_random(10)
-    print(first_fit(input))
-    print(best_fit(input))
-    print(first_fit_decreasing(input))
-    print(best_fit_decreasing(input))
+    func_list = [first_fit, best_fit, first_fit_decreasing, best_fit_decreasing]
+    results = [[], [], [], []]
+    for i in range(50):
+        input = generate_uniform(100)
+        for j in range(4):
+            results[j].append(len(func_list[j](input)))
+    for j in range(4):
+        print(np.mean(results[j]), end=' ')
+    print('')
+
+    results = [[], [], [], []]
+    for i in range(50):
+        input = generate_cubed(100)
+        for j in range(4):
+            results[j].append(len(func_list[j](input)))
+    for j in range(4):
+        print(np.mean(results[j]), end=' ')
+    print('')
+
+    results = [[], [], [], []]
+    for i in range(50):
+        input = generate_square_rooted(100)
+        for j in range(4):
+            results[j].append(len(func_list[j](input)))
+    for j in range(4):
+        print(np.mean(results[j]), end=' ')
+    print('')
+
+    results = [[], [], [], []]
+    for i in range(50):
+        input = generate_centered(100)
+        for j in range(4):
+            results[j].append(len(func_list[j](input)))
+    for j in range(4):
+        print(np.mean(results[j]), end=' ')
+
+    # print(first_fit(input))
+    # print(best_fit(input))
+    # print(first_fit_decreasing(input))
+    # print(best_fit_decreasing(input))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
